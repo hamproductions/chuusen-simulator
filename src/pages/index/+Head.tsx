ce732b33120ea998d44fbcb98d8b8849d4809cde
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2025-09-03 13:58:10 +0900

    fix: title

--- src/pages/index/+Head.tsx
+++ src/pages/index/+Head.tsx
@@ -2,5 +2,5 @@ import { t } from 'i18next';
 import { Metadata } from '~/components/layout/Metadata';
 
 export function Head() {
-  return <Metadata title={t('lottery.title')} />;
+  return <Metadata title={t('title')} />;
 }
